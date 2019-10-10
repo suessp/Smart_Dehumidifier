a07be3996b62e6908a5a1864fef59b86e915e734
suessp <suessp@oregonstate.edu>
2019-10-10 15:27:13 -0700

    added interrupt

--- Smart_Dehumidifier.ino
+++ Smart_Dehumidifier.ino
@@ -1,25 +1,24 @@
-/*
-ESP8266 Blink
-Blink the blue LED on the ESP8266 module
-*/
+
 
 #define LED 2 //Define blinking LED pin
 #define POT A0
 
-#define DIG1 0
-#define DIG2 1
-#define A 2
-#define B 3
-#define C 4
-#define D 5
-#define E 6
-#define F 7
-#define G 8
-
+#define DIG1 D0
+#define DIG2 D1
+#define A D2
+#define B D3
+#define C D4
+#define D D5
+#define E D6
+#define F D7
+#define G D8
+#define BUTTON GPIO 1
 int potValue = 0;
 
+volatile bool setFlag = false;
+
 int segPins[] = {A, B, C, D, E, F, G};
-byte Seg_Array[10][7] = {   //A B C D E F G
+byte seg_Array[10][7] = {   //A B C D E F G
                             { 0,0,0,0,0,0,1 },    // 0
                             { 1,0,0,1,1,1,1 },    // 1
                             { 0,0,1,0,0,1,0 },    // 2
@@ -33,7 +32,7 @@ byte Seg_Array[10][7] = {   //A B C D E F G
 void setup() {
 
 pinMode(LED, OUTPUT); // Initialize the LED pin as an output
-  pinMode(Potentiometer, INPUT);
+  pinMode(POT, INPUT);
   pinMode(DIG1, OUTPUT);
   pinMode(DIG2, OUTPUT);
   pinMode(A, OUTPUT);
@@ -43,30 +42,49 @@ pinMode(LED, OUTPUT); // Initialize the LED pin as an output
   pinMode(E, OUTPUT);
   pinMode(F, OUTPUT);
   pinMode(G, OUTPUT);
-  
+  attachInterrupt(digitalPinToInterrupt(BUTTON), interrupt, RISING);  
 }
+
+int startTime = 0;
+int endTime;
+int i = 0, j = 0;
+int d1 =0, d2 = 0;
 // the loop function runs over and over again forever
 void loop() {
-  //digitalWrite(LED, LOW); // Turn the LED on (Note that LOW is the voltage level)
-  //delay(1000); // Wait for a second
-  //digitalWrite(LED, HIGH); // Turn the LED off by making the voltage HIGH
-  //delay(1000); // Wait for two seconds
-  potValue = map(analogRead(POT), 1, 1023, 0, 9);
-  refreshDisplay(potValue);
-  //SevenSegPrint(DIG1, 9);
+
+  for(int i=0; i<32; i++){
+    potValue += analogRead(POT);
+  }
+  potValue = potValue/32;
+  potValue = map(analogRead(POT), 1, 1023, 0, 99);
+  d1 = potValue%10;
+  d2 = potValue/10;
+  refreshDisplay(d2, d1);
+
+   endTime = millis();                  // increments the counter approximately once a second
+   if ((endTime - startTime) >= 1000)
+   {
+      potValue = potValue/32;
+      potValue = map(analogRead(POT), 1, 1023, 0, 99);
+      d1 = potValue%10;
+      d2 = potValue/10;
+   }
+  delay(10);
+  refreshDisplay(d2, d1);
 }
 
 
 //implements digit multiplexing
-void refreshDisplay(int num){
-  int digit0 = num%10;
-  int digit1 = num/10;
+void refreshDisplay(int digit1,int digit0){
   digitalWrite(DIG1, LOW);                   //display digit 1
   digitalWrite(DIG2, HIGH);
   setSevenSeg(digit0);
   delay(5);
-  digitalWrite(DIG1, HIGH);                  //display digit 2
+  digitalWrite(DIG1, HIGH);
   digitalWrite(DIG2, LOW);
+  if(digit1==0){
+    digitalWrite(DIG2, HIGH);
+  }
   setSevenSeg(digit1);
   delay(5);
 }
@@ -75,7 +93,16 @@ void refreshDisplay(int num){
 void setSevenSeg(int number){
   int pin = 2; //begining pin starting at A.
   for(int i=0; i<7; i++){
-    digitalWrite(pin, num_array[number][i]);
+    digitalWrite(pin, seg_Array[number][i]);
     pin++;
+  }
+}
+
+void interrupt(){
+  if(setFlag){
+    setFlag = false;
+  }
+  else{
+    setFlag = true;
   }
 }
