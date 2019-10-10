4728f7ab8b81e601b605697d95bfe1cfb7041a32
suessp <suessp@oregonstate.edu>
2019-10-09 22:40:32 -0700

    Implemented LCD multiplexing. UNTESTED

--- Smart_Dehumidifier.ino
+++ Smart_Dehumidifier.ino
@@ -4,7 +4,7 @@ Blink the blue LED on the ESP8266 module
 */
 
 #define LED 2 //Define blinking LED pin
-#define Potentiometer A0
+#define POT A0
 
 #define DIG1 0
 #define DIG2 1
@@ -16,8 +16,9 @@ Blink the blue LED on the ESP8266 module
 #define F 7
 #define G 8
 
-int potBits = 0;
+int potValue = 0;
 
+int segPins[] = {A, B, C, D, E, F, G};
 byte Seg_Array[10][7] = {   //A B C D E F G
                             { 0,0,0,0,0,0,1 },    // 0
                             { 1,0,0,1,1,1,1 },    // 1
@@ -50,31 +51,29 @@ void loop() {
   //delay(1000); // Wait for a second
   //digitalWrite(LED, HIGH); // Turn the LED off by making the voltage HIGH
   //delay(1000); // Wait for two seconds
-  potBits = analogRead(Potentiometer);
-  SevenSegPrint(DIG1, map(potBits, 1, 1024, 0, 10));
+  potValue = map(analogRead(POT), 1, 1023, 0, 9);
+  refreshDisplay(potValue);
   //SevenSegPrint(DIG1, 9);
 }
 
-void refreshDisplay(int digit1, int digit0){
 
+//implements digit multiplexing
+void refreshDisplay(int num){
+  int digit0 = num%10;
+  int digit1 = num/10;
+  digitalWrite(DIG1, LOW);                   //display digit 1
+  digitalWrite(DIG2, HIGH);
+  setSevenSeg(digit0);
+  delay(5);
+  digitalWrite(DIG1, HIGH);                  //display digit 2
+  digitalWrite(DIG2, LOW);
+  setSevenSeg(digit1);
+  delay(5);
 }
 
-void SevenSegPrint(int digit, int number){
-  digitalWrite(digit, LOW);
-  int pin = 2;
-
-  int num_array[10][7] = {  //A B C D E F G
-                            { 0,0,0,0,0,0,1 },    // 0
-                            { 1,0,0,1,1,1,1 },    // 1
-                            { 0,0,1,0,0,1,0 },    // 2
-                            { 0,0,0,0,1,1,0 },    // 3
-                            { 1,0,0,1,1,0,0 },    // 4
-                            { 0,1,0,0,1,0,0 },    // 5
-                            { 0,1,0,0,0,0,0 },    // 6
-                            { 0,0,0,1,1,1,1 },    // 7
-                            { 0,0,0,0,0,0,0 },    // 8
-                            { 0,0,0,1,1,0,0 }};   // 9
-
+//sets segment pins
+void setSevenSeg(int number){
+  int pin = 2; //begining pin starting at A.
   for(int i=0; i<7; i++){
     digitalWrite(pin, num_array[number][i]);
     pin++;
